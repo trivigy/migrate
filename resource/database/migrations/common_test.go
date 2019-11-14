e01b27255c9979eca8362da57a61759e1802dc27
Konstantin Itskov <konstantin.itskov@kovits.com>
2019-11-14 18:47:26 -0500

    Rename driver to drivers package to reorganize

--- resource/database/migrations/common_test.go
+++ resource/database/migrations/common_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
-	"github.com/trivigy/migrate/v2/driver/docker"
+	"github.com/trivigy/migrate/v2/drivers/docker"
 	"github.com/trivigy/migrate/v2/types"
 )
 
